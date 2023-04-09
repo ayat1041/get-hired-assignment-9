57d630d44b1dd4844c223e8e66f544b0d743ae17
Ayat Rahman <57280365+ayat1041@users.noreply.github.com>
2023-04-10 01:47:10 +0600

    home banner done

--- src/components/HeaderCover/HeaderCover.jsx
+++ src/components/HeaderCover/HeaderCover.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./HeaderCover.css";
 const HeaderCover = () => {
-  return (
+  return ( 
     <div className="banner">
       <div className="banner__info">
         <h1>
           One Step<br></br>Closer To Your<br></br>
           <span>Dream Job</span>
         </h1>
+        <p>Explore thousands of job opportunities with all the<br></br>information you need. Its your future. Come find it. Manage all<br></br>your job application from start to finish.</p>
+        <button>Get Started</button>
       </div>
       <img src="person.png" alt="" />
     </div>
