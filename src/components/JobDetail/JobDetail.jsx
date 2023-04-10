dbe36fa135f13500b9f5fbf788c3193ab70dcd78
Ayat Rahman <57280365+ayat1041@users.noreply.github.com>
2023-04-10 16:28:03 +0600

    details page until cover done

--- src/components/JobDetail/JobDetail.jsx
+++ src/components/JobDetail/JobDetail.jsx
@@ -8,10 +8,31 @@ const JobDetail = () => {
   let jobsData = useLoaderData(); 
 
   jobsData = jobsData.find(job => job.id == jobId);
+  const {id,job_description,job_responsibility,educational_requirements,experiences,salary,job_title,contact_information,location} = jobsData;
 //   const currentJobs = jobsData.map(job => console.log(typeof job.id));
 
   return (
     <div className="job-details-container">
+        <div className="job-details-left">
+            <h1><span>Job Description: </span>{job_description}</h1>
+            <h1><span>Job Responsibility: </span>{job_responsibility}</h1>
+            <h1><span>Educational Requirements: </span><br></br>{educational_requirements}</h1>
+            <h1><span>Experiences: </span><br></br>{experiences}</h1>
+        </div>
+        <div className="job-details-right-container">
+            <div className="job-details-right">
+            <h2>Job Details</h2>
+            <hr></hr>
+            <h3><span>Salary:</span> {salary} (Per Month)</h3>
+            <h3><span>Job Title:</span> {job_title}</h3>
+            <h2>Contact Information</h2>
+            <hr></hr>
+            <h3><span>Phone:</span> {contact_information.phone}</h3>
+            <h3><span>Email:</span> {contact_information.email}</h3>
+            <h3><span>Address:</span> {location}</h3>
+            </div>
+            <button>Apply Now</button>
+        </div>
     </div>
   );
 };
